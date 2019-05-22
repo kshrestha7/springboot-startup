eb1dfffae8d864ebde036e90f2c1dbd498e6f009
kshrestha7 <kesha.shrestha0@gmail.com>
2019-05-21 22:54:45 -0400

    adding demo project

---
+++ src/test/java/com/project/springbootstartup/SpringbootStartupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.springbootstartup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootStartupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
