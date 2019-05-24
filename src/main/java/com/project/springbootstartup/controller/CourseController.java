4d4358dcf487d4cd1299c973832a997a2a61f44c
tuladharami <ashishtuladhar.job@gmail.com>
2019-05-23 20:07:07 -0500

    Added controller and model object.

--- src/main/java/com/project/springbootstartup/controller/CourseController.java
+++ src/main/java/com/project/springbootstartup/controller/CourseController.java
@@ -1,5 +1,24 @@
 package com.project.springbootstartup.controller;
 
+import com.project.springbootstartup.model.CourseModel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CourseController {
     // your course controller goes here
+
+@RequestMapping("/course")
+    public List<CourseModel> getAllCourse(){
+        CourseModel cm1 = new CourseModel("1","Computer","Learn Computer");
+        CourseModel cm2 = new CourseModel("2","Java","Spring Boot");
+
+        List<CourseModel> allCourse = new ArrayList<>();
+        allCourse.add(cm1);
+        allCourse.add(cm2);
+        return allCourse;
+    }
 }
--- src/main/java/com/project/springbootstartup/model/CourseModel.java
+++ src/main/java/com/project/springbootstartup/model/CourseModel.java
@@ -2,4 +2,38 @@ package com.project.springbootstartup.model;
 
 public class CourseModel {
     // your course model goes here
+
+    private String courseId;
+    private String courseName;
+    private String courseDescription;
+
+    public CourseModel(String courseId, String courseName, String courseDescription) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.courseDescription = courseDescription;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
 }
