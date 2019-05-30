ceab9b099bbd71a4327b37dbe7059f69cb0b9a6a
Kesha <kesha.shrestha0@gmail.com>
2019-05-29 20:58:41 -0400

    updated controller and service

--- src/main/java/com/project/springbootstartup/controller/CourseController.java
+++ src/main/java/com/project/springbootstartup/controller/CourseController.java
@@ -1,6 +1,8 @@
 package com.project.springbootstartup.controller;
 
 import com.project.springbootstartup.model.CourseModel;
+import com.project.springbootstartup.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +12,18 @@ import java.util.List;
 @RestController
 public class CourseController {
     // your course controller goes here
+    @Autowired
+    private CourseService courseService;
 
 @RequestMapping("/course")
     public List<CourseModel> getAllCourse(){
-        CourseModel cm1 = new CourseModel("1","Computer","Learn Computer");
-        CourseModel cm2 = new CourseModel("2","Java","Spring Boot");
 
-        List<CourseModel> allCourse = new ArrayList<>();
-        allCourse.add(cm1);
-        allCourse.add(cm2);
-        return allCourse;
+        return courseService.getAllCourses();
+    }
+
+    @RequestMapping("/courseId")
+    public CourseModel getCourseById(){
+
+        return courseService.getCourse();
     }
 }
--- src/main/java/com/project/springbootstartup/service/CourseService.java
+++ src/main/java/com/project/springbootstartup/service/CourseService.java
@@ -1,5 +1,30 @@
 package com.project.springbootstartup.service;
 
+import com.project.springbootstartup.model.CourseModel;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CourseService {
+
+    public List<CourseModel> createCourses(){
+        return null;
+    }
+    public List<CourseModel> getAllCourses(){
+
+        return null;
+      /*  return CourseModel cm1 = new CourseModel("1","Computer","Learn Computer");
+        CourseModel cm2 = new CourseModel("2","Java","Spring Boot");
+
+        List<CourseModel> allCourse = new ArrayList<>();
+        allCourse.add(cm1);
+        allCourse.add(cm2);;*/
+    }
+
+    public CourseModel getCourse() {
+        return null;
+    }
     // Your Course Service Goes here
+
 }
